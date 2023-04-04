e277653afdcb1ed24970c229dd8f5614bcefb84e
Marcos Cunha <marcos.cunha@ffit.com.br>
2023-04-04 20:34:12 -0300

    Implementado fake template

--- js/toggle.js
+++ js/toggle.js
@@ -4,9 +4,11 @@ function toggleCongratulations(el){
     document.getElementById('dreamgirl-post').style.display = 'none';
     var display = document.getElementById(el).style.display;
     if(display == 'none'){
+        document.getElementById('fake-template').style.display = 'none';
         document.getElementById(el).style.display = 'block';
     }else {
         document.getElementById(el).style.display = 'none';
+        document.getElementById('fake-template').style.display = 'block';
     }
 }
 
@@ -16,9 +18,11 @@ function toggleDreamgirl(el){
     document.getElementById('congratulations-post').style.display = 'none';
     var display = document.getElementById(el).style.display;
     if(display == 'none'){
+        document.getElementById('fake-template').style.display = 'none';
         document.getElementById(el).style.display = 'block';
     }else {
         document.getElementById(el).style.display = 'none';
+        document.getElementById('fake-template').style.display = 'block';
     }
 }
 
@@ -27,6 +31,7 @@ function toggleTimeline(el){
     document.getElementById('congratulations-post').style.display = 'none';
     var display = document.getElementById(el).style.display;
     if(display == 'none'){
+        document.getElementById('fake-template').style.display = 'none';
         document.getElementById(el).style.display = 'block';
     }else {
         document.getElementById(el).style.display = 'none';
@@ -38,10 +43,12 @@ function toggleSecret(el){
     document.getElementById('congratulations-post').style.display = 'none';
     var display = document.getElementById(el).style.display;
     if(display == 'none'){
+        document.getElementById('fake-template').style.display = 'none';
         document.getElementById(el).style.display = 'block';
     }else {
         document.getElementById(el).style.display = 'none';
         document.getElementById('timeline-post').style.display = 'none';
+        document.getElementById('fake-template').style.display = 'block';
     }
 }
 
